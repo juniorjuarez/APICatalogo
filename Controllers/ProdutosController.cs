e6d9a750ca1352ebb778cc6b35f2903e831efa38
Jose Juarez <juniojuarez@yahoo.com.br>
2025-07-31 18:45:38 +0000

    Criação do Crud

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,6 +1,8 @@
 using CatalogoProdutos.API.Context;
 using CatalogoProdutos.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace APICatalogo.Controllers
 {
@@ -26,9 +28,66 @@ namespace APICatalogo.Controllers
             }
             else
             {
-
-                return NotFound("Nenhum produto encontrado.");
+                return NotFound("Nenhum produto encontrado!");
             }
+        }
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto == null)
+            {
+                return NotFound("Nenhum produto encontrado!");
+            }
+            else
+            {
+                return produto;
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest(); ;
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(produto);
+        }
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto == null)
+            {
+                return NotFound("Nenhum produto encontrado!");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok();
+
         }
 
 
